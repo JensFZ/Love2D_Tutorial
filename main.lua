892e86493c283f2a5e12abd232a7e48c788a22a3
Jens Fischer <JensFZ@gmail.com>
2022-01-25 23:46:28 +0100

    player control hinzugefügt

--- main.lua
+++ main.lua
@@ -1,14 +1,22 @@
 debug = true
-playerImg = nil -- Player Image
+player = { x= 200, y= 720, speed = 150, img = nil}
 
 function love.load(arg)
-    playerImg = love.graphics.newImage('assets/Aircraft_03.png')
+    player.img = love.graphics.newImage('assets/Aircraft_03.png')
 end
 
 function love.update(dt)
+    if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+    end 
 
+    if love.keyboard.isDown('left', 'a') then
+        player.x = player.x - (player.speed*dt)
+    elseif love.keyboard.isDown('right', 'd') then 
+        player.x = player.x + (player.speed*dt)
+    end 
 end
 
 function love.draw(dt)
-    love.graphics.draw(playerImg, 100, 100)
+    love.graphics.draw(player.img, player.x, player.y)
 end
