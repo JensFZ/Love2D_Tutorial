d1ab31fe0f7a3d1846c725754b651bc4d8cd7987
Jens Fischer <JensFZ@gmail.com>
2022-01-26 00:41:02 +0100

    Build script added

--- Build.bat
+++ Build.bat
@@ -1,1 +1,4 @@
-"c:\Program Files\WinRAR\WinRAR.exe" a -afzip ..\Build\game.love *.lua "assets"
+rem Build final exe file
+"c:\Program Files\WinRAR\WinRAR.exe" a -afzip "game.love" *.lua "assets"
+copy /b build\love.exe+game.love build\game.exe
+del game.love
